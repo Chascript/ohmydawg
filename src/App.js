7c2dc1c09bfcc083fecbf5e558d94bba3fbec6e5
arcticsurgeon <conanc1993@gmail.com>
2020-09-23 13:45:16 +0100

    themes are now in themes.js not App.js

--- src/App.js
+++ src/App.js
@@ -3,48 +3,13 @@ import './App.css';
 import SignInSide from './components/sign-in-side';
 import Container from '@material-ui/core/Container';
 import DenseAppBar from './components/navigation-bar';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import './App.css'
-import { cyan, indigo } from '@material-ui/core/colors';
-
-
-const font = ['Mali']
-
+import { ThemeProvider } from '@material-ui/core/styles';
+import {darkTheme, lightTheme} from './themes'
 
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
 
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: "dark",
-      primary: {
-        light: '#757ce8',
-        main: '#000000', // background nav bar
-        dark: '#002884',
-        contrastText: '#ffffff', //text
-      },
-      secondary: {
-        light: '#cfcfcf', //
-        main: '#707070', //switch button
-        dark: '#ba000d',
-        contrastText: '#000',
-      },
-    },
-    typography:{
-        fontFamily: font,
-    }
-  });
-
-  const lightTheme = createMuiTheme({
-    palette: {
-      primary: indigo,
-      secondary: cyan,
-    },
-    typography:{
-        fontFamily: font,
-    }
-  });
     
   return (
     <ThemeProvider theme = {darkMode ? darkTheme : lightTheme} >
--- src/themes.js
+++ src/themes.js
@@ -1,17 +1,38 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import './App.css'
+import { cyan, indigo } from '@material-ui/core/colors';
 
 const font = ['Mali']
 
-const theme = createMuiTheme({
+export const darkTheme = createMuiTheme({
   palette: {
+    type: "dark",
     primary: {
-      main: '#424242',
-    }
+      light: '#757ce8',
+      main: '#000000', // background nav bar
+      dark: '#002884',
+      contrastText: '#ffffff', //text
+    },
+    secondary: {
+      light: '#cfcfcf', //
+      main: '#707070', //switch button
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
   },
   typography:{
       fontFamily: font,
   }
 });
 
-export default theme
+export const lightTheme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: cyan,
+  },
+  typography:{
+      fontFamily: font,
+  }
+});
+
+
