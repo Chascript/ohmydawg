aa2c49060e0b9771ccd3a20f6f87e52075ac1e50
arcticsurgeon <conanc1993@gmail.com>
2020-10-21 18:35:48 +0100

    files deleted, mistake made uploading

--- votebutton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
-
-export default function VoteButton(props) {
-  return(
-    <ThumbUpAltIcon onClick={props.voteForDog} />
-  )
-}
--- votenumbertally.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
-
-export default function VoteNumber(props) {
-
-  return (
-    <Typography>Total Votes: {props.voteNumber}</Typography>
-  )
-}
